fbf06d14239fda2a2158c10f3b8a3bcdb95f56fb
Virghileanu Teodor <teo.virghi@yahoo.ro>
2021-11-30 00:37:18 +0200

    minesweeper config panel

--- src/components/Minesweeper/Config.vue
+++ src/components/Minesweeper/Config.vue
@@ -1,82 +1,49 @@
 <script setup lang="ts">
-const keep = ref('')
+const rows = ref(9)
+const cols = ref(9)
+const bombs = ref(10)
 </script>
 
 <template>
-  <div class="px-5 py-2 rounded-lg shadow-emerald-200 shadow-md flex flex-row gap-2">
-    <AltButton>
-      AltButton
+  <div class="flex flex-row gap-1">
+    <div class="px-5 py-2 border border-gray-700 rounded-lg flex flex-row gap-2 items-center">
+      <span>Grid </span>
+      <div class="h-full w-px bg-black dark:bg-white opacity-10" />
+      <PrimaryInput
+        v-model.number="rows"
+        type="number"
+        class="w-22"
+      >
+        <template #label>
+          Rows
+        </template>
+      </PrimaryInput>
+      <span>x</span>
+      <PrimaryInput
+        v-model.number="cols"
+        type="number"
+        class="w-22"
+      >
+        <template #label>
+          Cols
+        </template>
+      </PrimaryInput>
+    </div>
+    <div class="px-5 py-2 border border-gray-700 rounded-lg flex flex-row gap-2 items-center">
+      <span>Bombs </span>
+      <div class="h-full w-px bg-black dark:bg-white opacity-10" />
+      <PrimaryInput
+        v-model.number="bombs"
+        type="number"
+        class="w-22"
+      >
+        <template #label>
+          Count
+        </template>
+      </PrimaryInput>
+    </div>
+    <AltButton class="px-5 py-2 rounded-lg">
+      Reset
     </AltButton>
-    <DangerButton>
-      DangerButton
-    </DangerButton>
-    <PrimaryButton>
-      PrimaryButton
-    </PrimaryButton>
-    <SuccessButton>
-      SuccessButton
-    </SuccessButton>
-    <WarnButton>
-      WarnButton
-    </WarnButton>
   </div>
-  <div class="px-5 py-2 rounded-lg shadow-emerald-200 shadow-md flex flex-row gap-2">
-    <AltButton
-      :is-tiny="true"
-    >
-      Alt
-    </AltButton>
-    <DangerButton
-      :is-tiny="true"
-    >
-      Danger
-    </DangerButton>
-    <PrimaryButton
-      :is-tiny="true"
-    >
-      Primary
-    </PrimaryButton>
-    <SuccessButton
-      :is-tiny="true"
-    >
-      Success
-    </SuccessButton>
-    <WarnButton
-      :is-tiny="true"
-    >
-      Warn
-    </WarnButton>
-  </div>
-  <div class="px-5 py-2 rounded-lg shadow-emerald-200 shadow-md flex flex-col gap-2">
-    <AltInput v-model="keep">
-      <template #label>
-        AltInput
-      </template>
-    </AltInput>
-    <DangerInput v-model="keep">
-      <template #label>
-        DangerInput
-      </template>
-    </DangerInput>
-    <PrimaryInput v-model="keep">
-      <template #label>
-        PrimaryInput
-      </template>
-    </PrimaryInput>
-    <SuccessInput v-model="keep">
-      <template #label>
-        SuccessInput
-      </template>
-    </SuccessInput>
-    <WarnInput v-model="keep">
-      <template #label>
-        WarnInput
-      </template>
-    </WarnInput>
-  </div>
-  <PrimaryInput type="file">
-    <template #label>
-      File Input
-    </template>
-  </PrimaryInput>
 </template>
